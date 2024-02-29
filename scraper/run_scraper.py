e1151b240f3d83af11c46afe5edbb7689780917f
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2024-02-29 12:37:00 -0500

    Change config path from an absolute path to a local path

--- scraper/run_scraper.py
+++ scraper/run_scraper.py
@@ -2,7 +2,7 @@ from selenium_scraper import WebScraper, WebScraperConfig
 import json
 
 def main():
-    config_path = "C:/text2mc/SCRUM-15/text2mc-dataprocessor/scraper/webscraper_config.json"
+    config_path = "webscraper_config.json"
     with open(config_path, 'r') as config_file:
         config = json.load(config_file)
 
