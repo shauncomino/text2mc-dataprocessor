e5cbf1d36bc3333e0083db0bee537d34ecea8c90
MorgueParty <thatonepersonnobodyknows@gmail.com>
2024-02-29 10:11:58 -0500

    Make scraper object-oriented: fix broken changes.

--- scraper/run_scraper.py
+++ scraper/run_scraper.py
@@ -1,17 +1,17 @@
-from scraper import WebScraper, WebScraperConfig
+from selenium_scraper import WebScraper, WebScraperConfig
 import json
 
 def main():
+    config_path = "C:/text2mc/SCRUM-15/text2mc-dataprocessor/scraper/webscraper_config.json"
     with open(config_path, 'r') as config_file:
         config = json.load(config_file)
 
     scraper_config = WebScraperConfig(**config)
-
     web_scraper = WebScraper(scraper_config)
 
     web_scraper.scrape_project_links() 
     web_scraper.scrape_project_download_links()
-    # web_scraper.get_image_descriptors()
+    web_scraper.get_build_descriptions()
 
 
 if __name__ == '__main__':
