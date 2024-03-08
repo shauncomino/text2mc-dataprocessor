1cf67d1e220b0fed7305d692361992129d5f1874
Shaun Comino <shauncomino@gmail.com>
2024-03-08 13:19:31 -0500

    again

--- scraper/run_scraper.py
+++ scraper/run_scraper.py
@@ -16,7 +16,7 @@ def main():
     web_scraper.scrape_project_links(pages_to_scrape=40)
     web_scraper.scrape_project_page_info()
     web_scraper.scrape_raw_map_download_links()
-    web_scraper.download_all_builds()
+    # web_scraper.download_all_builds()
 
 
 if __name__ == "__main__":
