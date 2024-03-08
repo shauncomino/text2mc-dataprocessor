4104498b204175f35af3b20211f2d83ab00f26d6
Steven Keppinger <steven.keppinger.jr@gmail.com>
2024-03-08 15:30:01 -0500

    small fix

--- world2vec/test.py
+++ world2vec/test.py
@@ -2,4 +2,4 @@ from world2vec import World2Vec
 
 build_chunks, superflat, superflat_surface = World2Vec.get_build_chunks("test_regions1")
 
-World2Vec.extract_build(build_chunks, superflat, 123)
+World2Vec.extract_build(build_chunks, superflat, superflat_surface, 123)
