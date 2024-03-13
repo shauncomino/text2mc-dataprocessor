4ddd11a9d1455b1dd90ff3ec2a1f1639d5838850
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2024-03-13 19:15:07 -0400

    Load a schematic file and print its information

--- schematic-loader/.idea/sonarlint/issuestore/index.pb
+++ schematic-loader/.idea/sonarlint/issuestore/index.pb
@@ -10,3 +10,5 @@ S
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+G
+src/main/java/Main.java,e\4\e44a8606ce010ad9b89aafdfcee2ab49afbf6d10
--- schematic-loader/src/main/java/org/text2mc/Main.java
+++ schematic-loader/src/main/java/org/text2mc/Main.java
@@ -1,7 +1,36 @@
 package org.text2mc;
 
+import net.sandrohc.schematic4j.SchematicLoader;
+import net.sandrohc.schematic4j.exception.ParsingException;
+import net.sandrohc.schematic4j.schematic.Schematic;
+import net.sandrohc.schematic4j.schematic.types.*;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String schemFilePath = "src/main/resources/12727.schematic";
+
+        try {
+            Schematic schematic = SchematicLoader.load(schemFilePath);
+            List<Pair<SchematicBlockPos, SchematicBlock>> blocks = schematic.blocks().collect(Collectors.toList());
+            List<SchematicBlockEntity> blockEntities = schematic.blockEntities().collect(Collectors.toList());
+            List<SchematicEntity> entities = schematic.entities().collect(Collectors.toList());
+
+            System.out.println(schematic.name());
+            System.out.println(schematic.width());
+            System.out.println(schematic.height());
+            System.out.println(schematic.length());
+            System.out.println();
+            System.out.println(blocks);
+            System.out.println();
+            System.out.println(blockEntities);
+            System.out.println();
+            System.out.println(entities);
+        } catch (ParsingException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
