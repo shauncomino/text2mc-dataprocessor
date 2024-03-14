534f4936e8dae1264bf22b68a2972509bb844de7
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2024-03-14 19:23:17 -0400

    Add a method to check if the current coordinate is valid

--- schematic-loader/src/main/java/org/text2mc/World.java
+++ schematic-loader/src/main/java/org/text2mc/World.java
@@ -2,16 +2,31 @@ package org.text2mc;
 
 public class World {
     private String[][][] worldArr;
+    private int width;
+    private int height;
+    private int length;
 
     public World(int width, int height, int length) {
         worldArr = new String[width][height][length];
+        this.width = width;
+        this.height = height;
+        this.length = length;
     }
 
     public String[][][] getWorldArr() {
         return worldArr;
     }
 
+    public boolean isValidCoordinate(int x, int y, int z) {
+        return x >= 0 && x < width &&
+                y >= 0 && y < height &&
+                z >= 0 && z < length;
+    }
+
     public void initializeCoordinate(String name, int x, int y, int z) {
+        if (!isValidCoordinate(x, y, z)) {
+            return;
+        }
         worldArr[x][y][z] = name;
     }
 
