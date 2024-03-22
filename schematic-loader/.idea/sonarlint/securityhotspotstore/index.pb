25b2fb9bd219ae9c104df2e6786bfa56ddc8eae2
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2024-03-21 21:37:12 -0400

    Iterate through schematic files and write the block name 3D array into a .out file

--- schematic-loader/.idea/sonarlint/securityhotspotstore/index.pb
+++ schematic-loader/.idea/sonarlint/securityhotspotstore/index.pb
@@ -12,3 +12,13 @@ T
 $src/main/java/org/text2mc/World.java,8\d\8d035290c4ad5eaae8ee5b8a2b315f59452995f5
 [
 +src/main/java/org/text2mc/WorldHandler.java,0\3\03dd9c69a5e15ddc91ea761c8fbe6751840b1e94
+Z
+*src/main/java/org/text2mc/FileHandler.java,0\e\0ea090add9384f2922ae091530c3420d12f8986b
+I
+src/main/output/12727.out,f\6\f69c935b397a28220fd9f8c790e21d0c450fcf15
+R
+"src/main/output/my_schematic_1.out,f\f\ffb34bb8033c034b5d7da42ea0855b05b475b6e8
+U
+%src/main/output/yellow-taxi-model.out,2\e\2ef853f4e8562b79f6fa806fb9879fa729032529
+K
+src/main/output/m35mako.out,9\4\947ec33bf43eacb1bab2f8804eeaefc04db04878
--- schematic-loader/src/main/java/org/text2mc/FileHandler.java
+++ schematic-loader/src/main/java/org/text2mc/FileHandler.java
@@ -1,2 +1,91 @@
-package org.text2mc;public class FileHandler {
+package org.text2mc;
+
+import net.sandrohc.schematic4j.schematic.Schematic;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHandler {
+    private String schemDirectoryPath;
+    private String outputDirectoryPath;
+    private String outputFileExtension;
+    private File directory;
+    private File[] schemFiles;
+
+    public FileHandler(String schemDirectoryPath, String outputDirectoryPath, String outputFileExtension) {
+        this.schemDirectoryPath = schemDirectoryPath;
+        this.outputDirectoryPath = outputDirectoryPath;
+        this.outputFileExtension = outputFileExtension;
+        this.directory = new File(schemDirectoryPath);
+        this.schemFiles = this.directory.listFiles();
+    }
+
+    public String getOutputFilePath(String schemFileName) {
+        return outputDirectoryPath + schemFileName + outputFileExtension;
+    }
+
+    private boolean fileExists(String filePath) {
+        File file = new File(filePath);
+        return file.exists();
+    }
+
+    public void exportSchematicFiles() {
+        if (schemFiles == null) {
+            System.out.println("No schematic files found");
+            return;
+        }
+
+        for (File schemFile : schemFiles) {
+            String schemFilePath = schemFile.getPath();
+            String schemFileName = FilenameUtils.removeExtension(schemFile.getName());
+            String outputFilePath = getOutputFilePath(schemFileName);
+
+            if (fileExists(outputFilePath)) {
+                continue;
+            }
+
+            SchematicHandler schematicHandler = new SchematicHandler(schemFilePath);
+            Schematic schematic = schematicHandler.getSchematic();
+            int width = schematic.width();
+            int height = schematic.height();
+            int length = schematic.length();
+
+            World world = new World(width, height, length);
+            WorldHandler worldHandler = new WorldHandler(world);
+            worldHandler.addBlocks(schematicHandler.getBlocks());
+            worldHandler.addBlockEntities(schematicHandler.getBlockEntities());
+            worldHandler.addEntities(schematicHandler.getEntities());
+
+            String[][][] worldArr = world.getWorldArr();
+            writeSchematicToFile(outputFilePath, worldArr);
+        }
+    }
+
+    private void writeSchematicToFile(String filePath, String[][][] worldArr) {
+        try {
+            File file = new File(filePath);
+
+            for (int x = 0; x < worldArr.length; x++) {
+                for (int y = 0; y < worldArr[x].length; y++) {
+                    for (int z = 0; z < worldArr[x][y].length; z++) {
+                        String blockName = worldArr[x][y][z];
+
+                        if (z != worldArr[x][y].length - 1) {
+                            blockName += " ";
+                        }
+
+                        FileUtils.write(file, blockName, "UTF-8", true);
+                    }
+                    FileUtils.write(file, "\n", "UTF-8", true);
+                }
+                FileUtils.write(file, "\n", "UTF-8", true);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 }
