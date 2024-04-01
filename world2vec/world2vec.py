47b744472a65d9da41cd2aee8612b58c4f20bfed
Greesh12 <greeshwanthp@gmail.com>
2024-04-01 13:00:21 -0400

    Added more naturally spawning blocks.

--- world2vec/world2vec.py
+++ world2vec/world2vec.py
@@ -103,6 +103,7 @@ class World2Vec:
                                             if block != None and anvil.Block.name(block) not in natural_blocks:
                                                 #print(anvil.Block.name(block))
                                                 build_chunks.append(chunk)
+                                                print(anvil.Block.name(block) + " found at coordinates", chunk.x*16, chunk.z*16)
                                                 if filename not in relevant_regions:
                                                     region_x = int(filename.split("r.")[1].split(".")[0])
                                                     region_z = int(filename.split("r.")[1].split(".")[1])
@@ -156,8 +157,8 @@ class World2Vec:
 
                 # Extract all the chunks from that cluster
                 cluster_chunks = [chunk for chunk, label in zip(build_chunks, labels) if label == cluster]
-                #for chunk in cluster_chunks:
-                    #print("Chunk found at coordinates", chunk.x, chunk.z)
+                for chunk in cluster_chunks:
+                    print("Chunk found at coordinates", chunk.x, chunk.z)
 
                 # Find the region files that contain the cluster_chunks from relevant_regions
 
