5aa3102b7dd9f5bc7100907a499573c89f48ef3c
Greesh12 <greeshwanthp@gmail.com>
2024-04-05 01:42:59 -0400

    Fixed Most Superflat world errors

--- world2vec/test.py
+++ world2vec/test.py
@@ -1,5 +1,5 @@
 from world2vec import World2Vec
 
-World2Vec.get_build("worlds/citadel/region", "citadel")
+World2Vec.get_build("worlds/plainhouse/region", "house")
 
 #World2Vec.export_json_to_npy("testjsons/m35mako.json", "testoutputs/m35mako.npy")
