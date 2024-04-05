22a9b9f24bf0fe86c03317c9a65e91bb3f1075ca
Steven Keppinger <steven.keppinger.jr@gmail.com>
2024-04-05 14:38:32 -0400

    fix for falsely identifying normal worlds as superflat

--- world2vec/test.py
+++ world2vec/test.py
@@ -1,5 +1,5 @@
 from world2vec import World2Vec
 
-World2Vec.get_build("worlds/towers/region", "towers")
+World2Vec.get_build("worlds/citadel/region", "citadel")
 
 #World2Vec.export_json_to_npy("testjsons/m35mako.json", "testoutputs/m35mako.npy")
