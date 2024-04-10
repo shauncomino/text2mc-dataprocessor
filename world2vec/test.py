5619efcab45f2d6e02bb386b29f9bbe92f6a5a2d
Greesh12 <greeshwanthp@gmail.com>
2024-04-10 13:42:07 -0400

    Added a fix for getting a section higher than intented.

--- world2vec/test.py
+++ world2vec/test.py
@@ -1,6 +1,6 @@
 from world2vec import World2Vec
 
-schem_list = World2Vec.get_build("worlds/union/region","worlds/union/schems", "union")
+schem_list = World2Vec.get_build("worlds/citadel/region","worlds/citadel/schems", "citadel")
 
 
 #World2Vec.export_json_to_npy("testjsons/m35mako.json", "testoutputs/m35mako.npy")
