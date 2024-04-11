4d54425854cce65d4f3aef2fde541c2de774b2b0
Shaun Comino <shauncomino@gmail.com>
2024-04-11 16:41:17 -0400

    Got it to handle archives correctly

--- scraper/run_scraper.py
+++ scraper/run_scraper.py
@@ -13,11 +13,11 @@ def main():
     scraper_config = WebScraperConfig(**config)
     web_scraper = WebScraper(scraper_config)
 
-    # web_scraper.scrape_project_links(pages_to_scrape=40)
+    web_scraper.scrape_project_links(pages_to_scrape=40)
     # web_scraper.scrape_project_page_info()
-    web_scraper.scrape_raw_map_download_links(
-        restart=False, download_when_extracted=False
-    )
+    # web_scraper.scrape_raw_map_download_links(
+    #     restart=False, download_when_extracted=False
+    # )
     # web_scraper.calculate_download_size_from_raw_links(restart=True)
     # web_scraper.download_all_builds()
 
--- world2vec/world2vecDriver.py
+++ world2vec/world2vecDriver.py
@@ -1,19 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 import pandas as pd
-from openai import OpenAI
 import time
-from pandas import DataFrame
 import os, shutil
 from dataclasses import dataclass, field
-from typeguard import typechecked
 from typing import Optional, List
-import requests
-from tqdm import tqdm
 import re
 import traceback
 from world2vec import World2Vec
@@ -24,8 +13,7 @@ import zipfile
 import numpy as np
 import h5py
 import glob
-from pyunpack import Archive
-from unrar import rarfile
+import patoolib
 
 
 @dataclass
@@ -82,13 +70,21 @@ class world2vecDriver:
 
         for i, row in dataframe.iloc[start_index:end_index].iterrows():
             try:
+                unique_name = f"batch_{batch_num}_{str(i)}"
                 filename = row["FILENAME"]
                 processed_paths = self.process_build(
                     filename,
-                    processed_file_name=f"batch_{batch_num}_{str(i)}",
+                    processed_file_name=unique_name,
                     temp_dir_path=temp_dir_path,
                 )
                 dataframe.at[i, "PROCESSED_PATHS"] = processed_paths
+                for path in processed_paths:
+                    self.convert_schemfile_to_hdf5(
+                        path,
+                        temp_dir_path,
+                        os.path.join(temp_dir_path, unique_name),
+                        unique_name,
+                    )
             except Exception as e:
                 print(e)
                 traceback.format_exc()
@@ -98,6 +94,7 @@ class world2vecDriver:
         filename: str,
         processed_file_name: str = "temp_schem",
         temp_dir_path: str = "temp",
+        straight_to_hdf5=False,
     ) -> List[str]:
         """
         Process a single build file and return the processed paths.
@@ -110,6 +107,8 @@ class world2vecDriver:
         if not os.path.exists(temp_dir_path):
             os.mkdir(temp_dir_path)
         try:
+            # Assuming this is part of a class with access to self.cfg.DOWNLOADED_BUILDS_FOLDER, etc.
+
             unprocessed_build_path = os.path.join(
                 self.cfg.DOWNLOADED_BUILDS_FOLDER, filename
             )
@@ -118,46 +117,63 @@ class world2vecDriver:
                     unprocessed_build_path, temp_dir_path
                 )
 
-                if (
-                    len(
-                        glob.glob(
-                            os.path.join(temp_dir_path, "/**/*.mca"), recursive=True
-                        )
-                    )
-                    > 0
+                # Search all files within the temporary directory once
+                all_files = glob.glob(
+                    os.path.join(temp_dir_path, "/**/*"), recursive=True
+                )
+
+                # Filter the search results by file extension
+                processed_paths = [
+                    f
+                    for f in all_files
+                    if f.endswith(".schem")
+                    or f.endswith(".schematic")
+                    or f.endswith(".mca")
+                ]
+
+                # If no '.schem' files, but there are '.schematic' files, use them
+                if not any(f.endswith(".schem") for f in processed_paths) and any(
+                    f.endswith(".schematic") for f in processed_paths
+                ):
+                    processed_paths = [
+                        f for f in processed_paths if f.endswith(".schematic")
+                    ]
+
+                # If neither '.schem' nor '.schematic' files are found, but '.mca' files are, convert them
+                if not processed_paths or all(
+                    f.endswith(".mca") for f in processed_paths
                 ):
                     schem_paths = self.convert_build_to_schemfile(
                         temp_dir_path, f"build_{processed_file_name}"
                     )
                     processed_paths = schem_paths
-
-                elif (
-                    len(
-                        glob.glob(
-                            os.path.join(temp_dir_path, "/**/*.schem"), recursive=True
-                        )
-                    )
-                    > 0
-                ):
-                    processed_paths = glob.glob(
-                        os.path.join(temp_dir_path, "/**/*.schem"), recursive=True
-                    )
-                elif len(
-                    glob.glob(
-                        os.path.join(temp_dir_path, "/**/*.schematic"), recursive=True
-                    )
-                ):
-                    processed_paths = glob.glob(
-                        os.path.join(temp_dir_path, "/**/*.schematic"), recursive=True
-                    )
-
-                self.delete_directory_contents(temp_dir_path)
-
-            elif filename.endswith(".schem"):
+            elif filename.endswith(".schematic") or filename.endswith(".schem"):
                 processed_paths = [filename]
-            else:
-                # If the file format is not supported, return an empty list
-                return processed_paths
+
+            if straight_to_hdf5:
+                new_paths = list()
+                for path in processed_paths:
+                    try:
+                        temp_json_path = os.path.join(
+                            temp_dir_path, f"{processed_file_name}.json"
+                        )
+                        hdf5_path = os.path.join(
+                            self.cfg.PROCESSED_BUILDS_FOLDER,
+                            f"{processed_file_name}.h5",
+                        )
+                        self.convert_schemfile_to_json(path, temp_json_path)
+                        npy_array = self.convert_json_to_npy(temp_json_path)
+                        self.convert_vector_to_hdf5(npy_array, hdf5_path)
+                        new_paths.append(hdf5_path)
+                    except Exception as e:
+                        print(
+                            f"Error processing schem to hdf5: {os.path.split(path)[-1]}"
+                        )
+                        print(e)
+                        traceback.format_exc()
+                processed_paths = new_paths
+
+            self.delete_directory_contents(temp_dir_path)
 
         except Exception as e:
             print(f"Error processing build {filename}: {e}")
@@ -168,13 +184,7 @@ class world2vecDriver:
     def extract_archive_to_temporary_directory(
         self, source_archive_path: str = None, outfolder_path: str = None
     ):
-        if source_archive_path.endswith(".zip"):
-            # For .zip files, use Archive from pyunpack
-            Archive(source_archive_path).extractall(outfolder_path)
-        elif source_archive_path.endswith(".rar"):
-            # For .rar files, use rarfile
-            with rarfile.RarFile(source_archive_path) as rf:
-                rf.extractall(outfolder_path)
+        patoolib.extract_archive(source_archive_path, outdir=outfolder_path)
 
     def delete_directory_contents(self, folder: str = None):
         for filename in os.listdir(folder):
@@ -212,11 +222,9 @@ class world2vecDriver:
     def convert_json_to_npy(self, json_file_path) -> np.ndarray:
         return World2Vec.export_json_to_npy(json_file_path)
 
-    def convert_vector_to_hdf5(self, vector, processed_file_prefix):
-        hdf5_out_path = os.path.join(
-            self.cfg.PROCESSED_BUILDS_FOLDER, f"{processed_file_prefix}.h5"
-        )
-        hdf5_file = h5py.File(f"{processed_file_prefix}.h5", "w")
+    def convert_vector_to_hdf5(self, vector, path):
+        hdf5_out_path = os.path.join(self.cfg.PROCESSED_BUILDS_FOLDER, f"{path}.h5")
+        hdf5_file = h5py.File(path, "w")
 
     def convert_schemfile_to_hdf5(
         self,
@@ -225,9 +233,9 @@ class world2vecDriver:
         json_file_path,
         processed_file_prefix,
     ):
-        self.convert_schemfile_to_json(self, schem_file_path, json_export_directory)
-        vector = self.convert_json_to_npy(self, json_file_path)
-        self.convert_vector_to_hdf5(self, vector, processed_file_prefix)
+        self.convert_schemfile_to_json(schem_file_path, json_export_directory)
+        vector = self.convert_json_to_npy(json_file_path)
+        self.convert_vector_to_hdf5(vector, processed_file_prefix)
 
     # Used to create processed and temporary directories
     def create_directory(self, dir_path: str):
@@ -291,17 +299,19 @@ def main():
 
     num_to_process = 5
 
-    # # Process a single .schem file
-    # print("Processings .schem files")
-    # schem_df = projects_df[projects_df["SUFFIX"] == ".schem"]
-    # for i, row in schem_df[0:num_to_process].iterrows():
-    #     world2vecdriver.process_build(row["FILENAME"], f"schem_test_{i}", r"D:\\temp")
+    # Process a single .schem file
+    print("Processings .schem files")
+    schem_df = projects_df[projects_df["SUFFIX"] == ".schem"]
+    for i, row in schem_df[0:num_to_process].iterrows():
+        world2vecdriver.process_build(
+            row["FILENAME"], f"schem_test_{i}", r"D:\\temp", straight_to_hdf5=True
+        )
 
     # Process a single .zip archive
-    print("Processing .zip files")
-    zip_df = projects_df[projects_df["SUFFIX"] == ".zip"]
-    for i, row in zip_df[0:num_to_process].iterrows():
-        world2vecdriver.process_build(row["FILENAME"], f"zip_test_{i}", r"D:\\temp")
+    # print("Processing .zip files")
+    # zip_df = projects_df[projects_df["SUFFIX"] == ".zip"]
+    # for i, row in zip_df[0:num_to_process].iterrows():
+    #     world2vecdriver.process_build(row["FILENAME"], f"zip_test_{i}", r"D:\\temp")
 
     # # Process a single .schematic file
     # print("Processing .schematic files")
