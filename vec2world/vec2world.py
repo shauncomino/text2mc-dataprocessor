012c95bfbb10e6c258937f6b8f0c0216b3352b78
Ryan Turner <ryanturner02@protonmail.com>
2024-07-28 19:14:35 -0400

    Fix existing folder directory error

--- vec2world/vec2world.py
+++ vec2world/vec2world.py
@@ -34,12 +34,14 @@ def convert_numpy_array_to_blocks(world_array):
 
     return world_array_blocks
 
+def trim_folder_path(folder_path):
+    return folder_path.strip().lstrip('/').rstrip('/')
+
 hdf5_file = sys.argv[1]
-schem_folder_path = "" if len(sys.argv) <= 2 else sys.argv[2]
+schem_folder_path = "" if len(sys.argv) <= 2 else trim_folder_path(sys.argv[2])
 schem_file_name = hdf5_file.removesuffix(".h5")
 
-if schem_folder_path != "" and not os.path.isdir(schem_folder_path):
-    schem_folder_path = schem_folder_path.strip().lstrip('/').rstrip('/')
+if schem_folder_path != "" and not os.path.exists(schem_folder_path):
     os.makedirs(schem_folder_path)
 
 integer_world_array = convert_hdf5_file_to_numpy_array(hdf5_file)
