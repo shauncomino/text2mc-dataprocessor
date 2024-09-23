6aefcf823fa0ad34ff0cde77a822b18c1f83f4f3
Greesh12 <greeshwanthp@gmail.com>
2024-09-23 18:28:56 -0400

    Wrote function to run the two embedded builds through the encoder. Function for interpolating between points is work in progress, need clarification before processing futher.

--- generative_model/predictor.py
+++ generative_model/predictor.py
@@ -5,6 +5,8 @@ import torch.nn as nn
 from encoder import text2mcVAEEncoder
 from decoder import text2mcVAEDecoder
 from text2mcVAEDataset import text2mcVAEDataset
+import scipy
+import numpy as np
 
 class text2mcPredictor(nn.Module):
     def __init__(self):
@@ -30,17 +32,17 @@ class text2mcPredictor(nn.Module):
         # self.decoder.load_state_dict(torch.load(self.DECODER_MODEL_PATH, weights_only=True))
         # self.decoder.eval()
 
-
     # 1. Loads two builds from the dataset (user specified)
     # 2. Embeds builds using trained embedding model
     def embed_builds(self, building1_path: str, building2_path: str):
         hdf5_files = [building1_path, building2_path]
 
-        print(hdf5_files)
-        print(self.BLOCK_TO_TOK)
-
         dataset = text2mcVAEDataset(file_paths=hdf5_files, block2embedding=self.embeddings, block2tok=self.block2tok, block_ignore_list=[102], fixed_size=(256, 256, 256, 32))
         
+        '''
+        Get the data and mask for the two builds by calling the __getitem__ method which converts the build into the embeddings.
+        Do we need the mask part for anything or is that is only for figuring out the loss?
+        '''
         building1_data, building1_mask = dataset.__getitem__(0)
         building2_data, building2_mask = dataset.__getitem__(1)
 
@@ -48,9 +50,53 @@ class text2mcPredictor(nn.Module):
 
     # 3. Sends the two embedded builds through the encoder portion of the VAE
     def encode_builds(self, embedding1, embedding2):
-        pass
+
+        '''
+        Get the latent points for the two builds
+        Do we need the mu and logvar for anything?
+        '''
+        z1, mu1, logvar1 = self.encoder(embedding1)
+        z2, mu2, logvar2 = self.encoder(embedding2)
+
+        return z1, z2
         
     # 4. Linearly interpolate between those n-dimensional latent points to get other latent points connecting the two
+    def interpolate_latent_points(self, z1, z2, num_steps):
+        """
+        Linearly interpolate between two n-dimensional latent points using LinearNDInterpolator.
+
+        Args:
+            point1 (np.ndarray): The first latent point (n-dimensional).
+            point2 (np.ndarray): The second latent point (n-dimensional).
+            num_steps (int): The number of interpolation steps.
+
+        Returns:
+            np.ndarray: An array of interpolated points.
+        """
+
+        """"
+            READ THIS WE NEED SOME HELP
+            what is z supposed to be?
+        """
+
+        # Ensure the points are numpy arrays
+        point1 = np.array(point1)
+        point2 = np.array(point2)
+
+        # Generate a series of values for t between 0 and 1
+        t_values = np.linspace(0, 1, num_steps)
+
+        # Create the interpolation function
+        points = np.array([[0], [1]])
+        values = np.vstack([point1, point2])
+        interpolator = scipy.LinearNDInterpolator(points, values)
+
+        # Compute the interpolated points
+        interpolated_points = interpolator(t_values[:, None])
+
+        return interpolated_points
+        pass
+
     # 5. Send those intermediate latent points through the decoder portion of the VAE    
     def call_vae(self):
         pass
@@ -64,13 +110,16 @@ class text2mcPredictor(nn.Module):
     def save_predicted_build(self):
         pass
 
-
 def main():
     building1_path = "rar_test5_Desert+Tavern+2.h5"
     building2_path = "rar_test6_Desert_Tavern.h5"
 
     predictor = text2mcPredictor()
-    predictor.embed_builds(building1_path, building2_path)
+    
+    building1_embedding, building2_embedding = predictor.embed_builds(building1_path, building2_path)
+    print(building1_embedding)
+    #building1_latent, building2_latent = predictor.encode_builds(building1_embedding, building2_embedding)
+    
 
 if __name__ == "__main__":
     main()
