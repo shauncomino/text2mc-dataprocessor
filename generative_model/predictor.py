9476e0d97d882654e0061c7839b8d5a25a573060
Greesh12 <greeshwanthp@gmail.com>
2024-09-23 17:33:20 -0400

    Succefully loaded embeddings and the models for the encoder and decoder. Then wrote a converter function to converted hdf5s to embeddings.
    Co-authored-by: Ryan <RyanTurner02@users.noreply.github.com>

--- generative_model/predictor.py
+++ generative_model/predictor.py
@@ -4,42 +4,51 @@ import torch
 import torch.nn as nn
 from encoder import text2mcVAEEncoder
 from decoder import text2mcVAEDecoder
-class text2mcPredictor(nn.Module):
-    EMBEDDINGS_FILE = "../block2vec/output/block2vec/embeddings.json"
-    EMBEDDING_MODEL_PATH = "../block2vec/checkpoints/current_model.pth"
+from text2mcVAEDataset import text2mcVAEDataset
 
+class text2mcPredictor(nn.Module):
     def __init__(self):
         super().__init__()
-        self.embeddings = json.load(open(self.EMBEDDINGS_FILE))
-        self.embedding_model = torch.load(self.EMBEDDING_MODEL_PATH, weights_only=True)
-        self.embedding_model.eval()
-        
-        #device_type = "cpu"
-        device_type = "cuda" if torch.cuda.is_available() else "cpu"
+        # Define the paths to the embeddings and models
+        self.EMBEDDINGS_FILE = "../block2vec/output/block2vec/embeddings.json"
+        self.EMBEDDING_MODEL_PATH = "../block2vec/checkpoints/best_model.pth"
 
-        device = torch.device(device_type)
-        self.encoder = text2mcVAEEncoder().to(device)
-        self.decoder = text2mcVAEDecoder().to(device)
+        self.ENCODER_MODEL_PATH = "generative_model/checkpoints/encoder.pth"
+        self.DECODER_MODEL_PATH = "generative_model/checkpoints/decoder.pth"
+
+        self.BLOCK_TO_TOK = "../world2vec/block2tok.json"
+
+        # Load the pre-trained embeddings and the encoder and decoder models and set them to eval mode
+        self.embeddings = json.load(open(self.EMBEDDINGS_FILE))
+        self.block2tok = json.load(open(self.BLOCK_TO_TOK))
+
+        # self.encoder = text2mcVAEEncoder()
+        # self.encoder.load_state_dict(torch.load(self.ENCODER_MODEL_PATH, weights_only=True))
+        # self.encoder.eval()
+
+        # self.decoder = text2mcVAEDecoder()
+        # self.decoder.load_state_dict(torch.load(self.DECODER_MODEL_PATH, weights_only=True))
+        # self.decoder.eval()
+
 
     # 1. Loads two builds from the dataset (user specified)
-    def load_two_builds_from_dataset(self, building1_path: str, building2_path: str):
-        building1 = h5.File(building1_path, 'r')
-        building2 = h5.File(building2_path, 'r')
-        return building1, building2
-
     # 2. Embeds builds using trained embedding model
-    def embed_builds(self, building1, building2):
-        embedding1 = self.embedding_model(building1)
-        embedding2 = self.embedding_model(building2)
-        return embedding1, embedding2
+    def embed_builds(self, building1_path: str, building2_path: str):
+        hdf5_files = [building1_path, building2_path]
 
-    def embed(self, building: h5.File):
-        return self.embedding_model(building)
-    
+        print(hdf5_files)
+        print(self.BLOCK_TO_TOK)
+
+        dataset = text2mcVAEDataset(file_paths=hdf5_files, block2embedding=self.embeddings, block2tok=self.block2tok, block_ignore_list=[102], fixed_size=(256, 256, 256, 32))
+        
+        building1_data, building1_mask = dataset.__getitem__(0)
+        building2_data, building2_mask = dataset.__getitem__(1)
+
+        return building1_data, building2_data
+
     # 3. Sends the two embedded builds through the encoder portion of the VAE
     def encode_builds(self, embedding1, embedding2):
-        encode1 = self.encoder(embedding1)
-        encode2 = self.encoder(embedding2)
+        pass
         
     # 4. Linearly interpolate between those n-dimensional latent points to get other latent points connecting the two
     # 5. Send those intermediate latent points through the decoder portion of the VAE    
@@ -57,15 +66,11 @@ class text2mcPredictor(nn.Module):
 
 
 def main():
-
     building1_path = "rar_test5_Desert+Tavern+2.h5"
     building2_path = "rar_test6_Desert_Tavern.h5"
 
     predictor = text2mcPredictor()
-    building1, building2 = predictor.load_two_builds_from_dataset(building1_path, building2_path)
-
-    print(building1)
-
+    predictor.embed_builds(building1_path, building2_path)
 
 if __name__ == "__main__":
     main()
