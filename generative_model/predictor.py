8fb2090298fd9307d7783233fe6a3a065667248f
Greesh12 <greeshwanthp@gmail.com>
2024-10-02 16:29:30 -0400

    Finished the predictor class. The embedded builds from the decoder are converted into blocks and saved into a schematic file.

--- generative_model/predictor.py
+++ generative_model/predictor.py
@@ -9,8 +9,9 @@ import scipy
 import numpy as np
 import os
 import h5py
+import vec2world
 from datetime import datetime
-
+from sklearn.neighbors import NearestNeighbors
 
 class text2mcPredictor(nn.Module):
     def __init__(self):
@@ -22,7 +23,6 @@ class text2mcPredictor(nn.Module):
         self.ENCODER_MODEL_PATH = "generative_model/checkpoints/encoder.pth"
         self.DECODER_MODEL_PATH = "generative_model/checkpoints/decoder.pth"
 
-        self.TOK_TO_BLOCK = "../world2vec/tok2block.json"
         self.BLOCK_TO_TOK = "../world2vec/block2tok.json"
 
         self.SAVE_DIRECTORY = "../generated_builds/"
@@ -30,7 +30,6 @@ class text2mcPredictor(nn.Module):
 
         # Load the pre-trained embeddings and the encoder and decoder models and set them to eval mode
         self.embeddings = json.load(open(self.EMBEDDINGS_FILE))
-        self.tok2block = json.load(open(self.TOK_TO_BLOCK))
         self.block2tok = json.load(open(self.BLOCK_TO_TOK))
 
         # self.encoder = text2mcVAEEncoder()
@@ -46,7 +45,7 @@ class text2mcPredictor(nn.Module):
     def embed_builds(self, building1_path: str, building2_path: str):
         hdf5_files = [building1_path, building2_path]
 
-        dataset = text2mcVAEDataset(file_paths=hdf5_files, block2embedding=self.embeddings, block2tok=self.block2tok, block_ignore_list=[102], fixed_size=(256, 256, 256, 32))
+        dataset = text2mcVAEDataset(file_paths=hdf5_files, block2embedding=self.embeddings, block2tok=self.block2tok, block_ignore_list=[102], fixed_size=(64, 64, 64))
         
         '''
         Get the data and mask for the two builds by calling the __getitem__ method which converts the build into the embeddings.
@@ -55,7 +54,7 @@ class text2mcPredictor(nn.Module):
         building1_data, building1_mask = dataset.__getitem__(0)
         building2_data, building2_mask = dataset.__getitem__(1)
 
-        return building1_data, building2_data
+        return building1_data, building2_data, dataset.embedding_matrix
 
     # 3. Sends the two embedded builds through the encoder portion of the VAE
     def encode_builds(self, embedding1, embedding2):
@@ -78,79 +77,36 @@ class text2mcPredictor(nn.Module):
             interpolations.append(z_interp)
 
         return interpolations
-
-    # 5. Send those intermediate latent points through the decoder portion of the VAE    
-    def call_vae(self, interpolations, save_dir):
-        
-        interp_builds_paths = []
-        
-        recon_build = self.decoder(interpolations)
-        # recon_build: (1, num_tokens, Depth, Height, Width)
-
-        # Convert logits to predicted tokens
-        recon_build = recon_build.argmax(dim=1)  # (1, Depth, Height, Width)
-
-        # Convert to numpy array
-        recon_build_np = recon_build.cpu().numpy().squeeze(0)  # (Depth, Height, Width)
-
-        # Save the build as an HDF5 file
-        save_path = os.path.join(save_dir, f'{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.h5')
-        with h5py.File(save_path, 'w') as h5f:
-            h5f.create_dataset('build', data=recon_build_np, compression='gzip')
-        
-        interp_builds_paths.append(save_path)
-
-        print(f'Saved interpolated build at {save_path}')
-        return interp_builds_paths
-
+    
     # 6. Convert those intermediate (embedded at this point) builds into tokens
-    def convert_intermediate_builds_to_tokens(self, interpolations):
+    def embeddings_to_tokens(self, embedded_data, embedding_matrix):
+        batch_size, embedding_dim, D, H, W = embedded_data.shape
+        N = D * H * W
+        embedded_data_flat = embedded_data.view(batch_size, embedding_dim, -1).permute(0, 2, 1).contiguous()
+        embedded_data_flat = embedded_data_flat[0].numpy()  # (N, Embedding_Dim)
+
+        nbrs = NearestNeighbors(n_neighbors=1, algorithm='auto').fit(embedding_matrix)
+        distances, indices = nbrs.kneighbors(embedded_data_flat)
+        tokens = indices.flatten().reshape(D, H, W)
+        return tokens
+    
+    # 5. Send those intermediate latent points through the decoder portion of the VAE    
+    def call_vae(self, interpolations, embedding_matrix):
         
-        tokens = list(self.tok2block.keys())
-        embeddings = np.array(list(self.embeddings.values()))
-
-        token_arrays = []
-
         for z in interpolations:
-            recon_build = self.decoder(z)
-            # recon_build: (1, num_tokens, Depth, Height, Width)
-
-            # Convert logits to predicted tokens
-            recon_build = recon_build.argmax(dim=1)  # (1, Depth, Height, Width)
+            recon_embedding = self.decoder(z)
 
             # Convert to numpy array
-            recon_build_np = recon_build.cpu().numpy().squeeze(0)  # (Depth, Height, Width)
+            recon_embedding = recon_embedding.cpu()
 
-            # Initialize an array to store the token representation
-            token_array = np.empty(recon_build_np.shape, dtype=object)
+            # Convert tokens to block names
+            tokens = text2mcPredictor.embeddings_to_tokens(recon_embedding, embedding_matrix) # 3d Array Of Integers
 
-            # Flatten the recon_build_np for distance computation
-            recon_build_flat = recon_build_np.reshape(-1, recon_build_np.shape[-1])
+            # Call functions from vec2world to convert tokens to blocks and same it as a schematics
+            string_world = vec2world.convert_numpy_array_to_blocks(tokens)
+            file_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+            vec2world.create_schematic_file(string_world, self.SAVE_DIRECTORY, file_name)
 
-            # Compute distances between recon_build_flat and pre-trained embeddings
-            distances = cdist(recon_build_flat, embeddings, metric='euclidean')
-
-            # Find the index of the closest embedding for each point
-            closest_indices = np.argmin(distances, axis=1)
-
-            # Map the closest indices to tokens
-            closest_tokens = np.array(tokens)[closest_indices]
-
-            # Reshape the token array to the original shape
-            token_array = closest_tokens.reshape(recon_build_np.shape)
-
-            token_arrays.append(token_array)
-
-        return token_arrays
-
-
-        pass
-    
-    # 7. Convert those token arrays into actual builds using vec2world
-    # 8. Save the build to be accessible somewhere
-    def save_predicted_build(self):
-        file_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        pass
 
 def main():
     building1_path = "rar_test5_Desert+Tavern+2.h5"
@@ -158,10 +114,10 @@ def main():
 
     predictor = text2mcPredictor()
     
-    building1_embedding, building2_embedding = predictor.embed_builds(building1_path, building2_path)
-    print(building1_embedding)
-    #building1_latent, building2_latent = predictor.encode_builds(building1_embedding, building2_embedding)
+    building1_embedding, building2_embedding, embedding_matrix = predictor.embed_builds(building1_path, building2_path)
+    building1_latent, building2_latent = predictor.encode_builds(building1_embedding, building2_embedding)
+    interpolations = predictor.interpolate_latent_points(building1_latent, building2_latent, num_interpolations=1)
+    predictor.call_vae(interpolations, embedding_matrix)
 
-    
 if __name__ == "__main__":
     main()
