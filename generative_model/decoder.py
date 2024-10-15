16b718329c604cae033bcb3c831c21352b585ae1
shauncomino <shauncomino@gmail.com>
2024-10-15 19:55:20 -0400

    Shape fixing

--- generative_model/decoder.py
+++ generative_model/decoder.py
@@ -4,7 +4,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-# Reuse AxialAttention3D and related classes from encoder.py
+# Reuse AxialAttentionBlock3D and text2mcVAEResidualBlock from encoder.py
 from encoder import AxialAttentionBlock3D, text2mcVAEResidualBlock
 
 class text2mcVAEDecoder(nn.Module):
@@ -13,31 +13,27 @@ class text2mcVAEDecoder(nn.Module):
         self.embedding_dim = embedding_dim
 
         self.initial_layers = nn.Sequential(
-            nn.ConvTranspose3d(8, 1024, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose3d(8, 1024, kernel_size=4, stride=4, padding=0),  # D=8 -> D=32
             text2mcVAEResidualBlock(1024, 1024),
             AxialAttentionBlock3D(1024),
             text2mcVAEResidualBlock(1024, 1024),
         )
         self.upsampling_layers = nn.Sequential(
-            nn.ConvTranspose3d(1024, 512, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose3d(1024, 512, kernel_size=2, stride=2, padding=0),  # D=32 -> D=64
             text2mcVAEResidualBlock(512, 512),
             text2mcVAEResidualBlock(512, 512),
-            nn.ConvTranspose3d(512, 256, kernel_size=4, stride=2, padding=1),
-            text2mcVAEResidualBlock(256, 256),
-            text2mcVAEResidualBlock(256, 256),
-            nn.ConvTranspose3d(256, 128, kernel_size=4, stride=2, padding=1),
-            text2mcVAEResidualBlock(128, 128),
-            text2mcVAEResidualBlock(128, 128),
         )
         self.shared_layers = nn.Sequential(
-            nn.GroupNorm(32, 128),
-            text2mcVAEResidualBlock(128, 128),
+            nn.GroupNorm(32, 512),
+            text2mcVAEResidualBlock(512, 512),
         )
+        # First head: embeddings
         self.embedding_head = nn.Sequential(
-            nn.Conv3d(128, embedding_dim, kernel_size=3, padding=1),
+            nn.Conv3d(512, embedding_dim, kernel_size=3, padding=1),
         )
+        # Second head: block vs. air prediction
         self.block_air_head = nn.Sequential(
-            nn.Conv3d(128, 1, kernel_size=3, padding=1),
+            nn.Conv3d(512, 1, kernel_size=3, padding=1),
             nn.Sigmoid()
         )
 
