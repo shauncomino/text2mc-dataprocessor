79dc04322f558556a45cf87be8c397a0235d18b2
shauncomino <shauncomino@gmail.com>
2024-10-17 12:49:34 -0400

    MORE

--- generative_model/train.py
+++ generative_model/train.py
@@ -220,7 +220,7 @@ def embedding_to_tokens(embeddings_pred, embedding_matrix):
     return tokens
 
 # Function to interpolate and generate builds
-def interpolate_and_generate(encoder, decoder, build1_path, build2_path, save_dir, epoch, num_interpolations=20):
+def interpolate_and_generate(encoder, decoder, build1_path, build2_path, save_dir, epoch, num_interpolations=40):
     encoder.eval()
     decoder.eval()
     with torch.no_grad():
