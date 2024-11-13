e59fe4454fbc2221274d9d31ceddb49064889f46
Greesh12 <greeshwanthp@gmail.com>
2024-11-13 14:07:28 -0500

    Added a script to print out all the paths of the gifs generated by the predictor.

--- generative_model/pick_build.py
+++ generative_model/pick_build.py
@@ -8,7 +8,7 @@ import gc
 builds1 = ['/lustre/fs1/groups/jaedo/processed_builds/batch_217_5637.h5',"/lustre/fs1/groups/jaedo/processed_builds/batch_225_5840.h5",'/lustre/fs1/groups/jaedo/processed_builds/batch_56_1440.h5',"/lustre/fs1/groups/jaedo/processed_builds/batch_319_8281.h5", "/lustre/fs1/groups/jaedo/processed_builds/batch_109_2825.h5"]
 builds2 = ["/lustre/fs1/groups/jaedo/processed_builds/batch_404_10483.h5",'/lustre/fs1/groups/jaedo/processed_builds/batch_3_67.h5','/lustre/fs1/groups/jaedo/processed_builds/batch_397_10302.h5','/lustre/fs1/groups/jaedo/processed_builds/batch_22_557.h5', '/lustre/fs1/groups/jaedo/processed_builds/batch_52_1334.h5']
 
-r = text2mcPredictor()
+predictor = text2mcPredictor()
 
 for build1_path in builds1:
     for build2_path in builds2:
--- rendering/glob_gifs.py
+++ rendering/glob_gifs.py
@@ -1,11 +1,14 @@
-import glob
 import os
 
 # Specify the path to the folder
 folder_path = "/lustre/fs1/groups/jaedo/generated_builds/"
 
 # Find all .gif files in the folder and subfolders
-gif_files = glob.glob(os.path.join(folder_path, '**', '*.gif'), recursive=True)
+gif_files = []
+for root, dirs, files in os.walk(folder_path):
+    for file in files:
+        if file.endswith('.gif'):
+            gif_files.append(os.path.join(root, file))
 
 # Print the file paths
 for file_path in gif_files:
